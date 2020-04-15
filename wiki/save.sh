c2aa9ab4ca695b24422f5350657ce6debb468e63
bentrevett <bentrevett@gmail.com>
2020-04-15 18:47:04 +0100

    ./save.sh

--- wiki/save.sh
+++ wiki/save.sh
@@ -3,3 +3,4 @@ cp index.html ~/Documents/github/bentrevett.github.io/wiki/
 cp save.sh ~/Documents/github/bentrevett.github.io/wiki/
 git -C ~/Documents/github/bentrevett.github.io/ add wiki/*
 git -C ~/Documents/github/bentrevett.github.io/ commit -m  "$0"
+git -C ~/Documents/github/bentrevett.github.io/ push
