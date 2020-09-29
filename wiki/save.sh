a05ff5d1a418f01336d128b6d197a94ee9cfcb37
Ben Trevett <bentrevett@gmail.com>
2020-09-29 19:42:24 +0100

    update

--- wiki/save.sh
+++
@@ -1,6 +0,0 @@
-cp -r images/ ~/Documents/github/bentrevett.github.io/wiki/
-cp index.html ~/Documents/github/bentrevett.github.io/wiki/
-cp save.sh ~/Documents/github/bentrevett.github.io/wiki/
-git -C ~/Documents/github/bentrevett.github.io/ add wiki/*
-git -C ~/Documents/github/bentrevett.github.io/ commit -m  "update wiki"
-git -C ~/Documents/github/bentrevett.github.io/ push
