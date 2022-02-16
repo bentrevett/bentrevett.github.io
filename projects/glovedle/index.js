29568a95811b5dfc4cd13219ee3ffabafbcf9fff
Ben Trevett <bentrevett@gmail.com>
2022-02-16 10:31:15 +0000

    refactor to use glove-embeddings.github.io

--- projects/glovedle/index.js
+++ projects/glovedle/index.js
@@ -18,55 +18,72 @@ function getCosineSimilarity(a, b) {
   return getDot(a, b) / (getMagnitude(a) * getMagnitude(b));
 }
 
-var guess = document.getElementById("guess");
-guess.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("submit_button").click();
+async function getWordVector(word) {
+  const url = `https://glove-embeddings.github.io/glove.6B.300d/${word}`;
+  const response = await fetch(url);
+  try {
+    return await response.json();
+  } catch (e) {
+    return null;
   }
-});
+}
 
-var submit_btn = document.getElementById("submit_button");
-submit_btn.addEventListener("click", function () {
-  var guess_word = document.getElementById("guess").value.toLowerCase();
-  var guess_vec = embeddings.get(guess_word);
-  if (typeof guess_vec === "undefined") {
-    result = document.getElementById("result");
-    result.innerHTML = `<strong>${guess_word}</strong> is not a valid word!`;
-    result.style.color = "red";
-  } else {
-    similarity = getCosineSimilarity(target_vec, guess_vec).toFixed(3);
-    result = document.getElementById("result");
-    if (guess_word == target_word) {
-      result.innerHTML = `<strong>${guess_word}</strong> is the correct word!`;
-      result.style.color = "green";
+async function main() {
+  let target_word =
+    target_words[Math.floor(Math.random() * target_words.length)];
+  console.log(`target word: ${target_word}`);
+
+  let target_vec = await getWordVector(target_word);
+  console.log(target_vec);
+
+  let guesses = [];
+
+  var guess = document.getElementById("guess");
+  guess.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("submit_button").click();
+    }
+  });
+
+  var submit_btn = document.getElementById("submit_button");
+  submit_btn.addEventListener("click", async function () {
+    var guess_word = document.getElementById("guess").value.toLowerCase();
+    var guess_vec = await getWordVector(guess_word);
+    if (guess_vec === null) {
+      result = document.getElementById("result");
+      result.innerHTML = `<strong>${guess_word}</strong> is not a valid word!`;
+      result.style.color = "red";
     } else {
-      result.innerHTML = `<strong>${guess_word}</strong> has a similarity of ${similarity}`;
-      result.style.color = "blue";
+      similarity = getCosineSimilarity(target_vec, guess_vec).toFixed(3);
+      result = document.getElementById("result");
+      if (guess_word == target_word) {
+        result.innerHTML = `<strong>${guess_word}</strong> is the correct word!`;
+        result.style.color = "green";
+      } else {
+        result.innerHTML = `<strong>${guess_word}</strong> has a similarity of ${similarity}`;
+        result.style.color = "blue";
+      }
+      if (
+        typeof guesses.map((x) => x.guess).find((x) => x == guess_word) ===
+        "undefined"
+      ) {
+        guesses.push({ guess: guess_word, similarity: similarity });
+        guesses.sort((a, b) => b.similarity - a.similarity);
+        var guesses_html_string = guesses
+          .map(
+            (a) =>
+              `${a.guess == target_word ? "<strong>" : ""}${a.guess}: ${
+                a.similarity
+              }${a.guess == target_word ? "</strong>" : ""}`
+          )
+          .join("<br>");
+        results = document.getElementById("results");
+        results.innerHTML = guesses_html_string;
+      }
     }
-    if (
-      typeof guesses.map((x) => x.guess).find((x) => x == guess_word) ===
-      "undefined"
-    ) {
-      guesses.push({ guess: guess_word, similarity: similarity });
-      guesses.sort((a, b) => b.similarity - a.similarity);
-      var guesses_html_string = guesses
-        .map(
-          (a) =>
-            `${a.guess == target_word ? "<strong>" : ""}${a.guess}: ${
-              a.similarity
-            }${a.guess == target_word ? "</strong>" : ""}`
-        )
-        .join("<br>");
-      results = document.getElementById("results");
-      results.innerHTML = guesses_html_string;
-    }
-  }
-  document.getElementById("guess").value = "";
-});
+    document.getElementById("guess").value = "";
+  });
+}
 
-var target_word = target_words[Math.floor(Math.random() * target_words.length)];
-var target_vec = embeddings.get(target_word);
-var guesses = [];
-
-console.log(`target word: ${target_word}`);
+main();
